af04c973dd7294eac5143f0d6347ec31f07b9146
Azl1337 <azat.azat.165@yandex.ru>
2020-04-28 23:34:01 +0300

    Добавьте файлы проекта.

---
+++ WPFHomeWork/MainWindow.xaml.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WPFHomeWork.Models;
+using System.Data.Entity;
+
+namespace WPFHomeWork
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        BuySellDBEntities db;
+        public MainWindow()
+        {
+            InitializeComponent();
+            LoadData();
+        }
+
+        void LoadData()
+        {
+            try
+            {
+                db = new BuySellDBEntities();
+                db.tWorker.Load();
+
+                dttWorker.ItemsSource = db.tWorker.Local.ToBindingList();
+            }
+            catch
+            {
+                MessageBox.Show("Ошибка");
+                db.Dispose();
+            }
+
+        }
+
+
+        private void BtnSave_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                db.SaveChanges();
+            }
+            catch
+            {
+                MessageBox.Show("Ошибка");
+                db.Dispose();
+            }
+        }
+
+        private void BtnDelete_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                if (dttWorker.SelectedItems.Count > 0)
+                {
+                    for (int i = 0; i < dttWorker.SelectedItems.Count; i++)
+                    {
+                        tWorker worker = dttWorker.SelectedItems[i] as tWorker;
+                        if (worker != null)
+                        {
+                            db.tWorker.Remove(worker);
+                        }
+                    }
+                }
+                db.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Ошибка");
+                db.Dispose();
+                LoadData();
+            }
+        }
+
+        private void BtnEdit_Click(object sender, RoutedEventArgs e)
+        {
+            if (dttWorker.SelectedItems.Count > 0)
+            {
+                tWorker worker = dttWorker.SelectedItem as tWorker;
+                if (worker == null)
+                    return;
+                WindowInformationEditor windowInfEdit = new WindowInformationEditor(worker, db);
+                windowInfEdit.ShowDialog();
+                db.Dispose();
+                LoadData();
+
+            }
+
+        }
+    }
+}
---
+++ WPFHomeWork/WindowInformationEditor.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WPFHomeWork.Models;
+using System.Data.Entity;
+
+namespace WPFHomeWork
+{
+    /// <summary>
+    /// Логика взаимодействия для WindowInformationEditor.xaml
+    /// </summary>
+    public partial class WindowInformationEditor : Window
+    {
+        BuySellDBEntities db;
+        tWorker sp;
+        public WindowInformationEditor()
+        {
+            InitializeComponent();
+        }
+
+        public WindowInformationEditor(tWorker worker, BuySellDBEntities d)
+        {
+            InitializeComponent();
+            db = d;
+            sp = worker;
+            tbFIO.Text = worker.sFIO;
+            txAddress.Text = worker.address;
+            dtpBithday.SelectedDate = worker.bithday;
+            tbphone.Text = worker.phone;
+            tbbase.Text = worker.base_worker;
+            tbexp.Text = Convert.ToString(worker.experience);
+            tbsp.Text = worker.special;
+            tbprice.Text = Convert.ToString(worker.price);
+            tbpas.Text = Convert.ToString(worker.passport);
+        }
+
+        private void BtnSAve_Click(object sender, RoutedEventArgs e)
+        {
+            sp.sFIO = tbFIO.Text;
+            sp.passport = tbpas.Text;
+            sp.address = txAddress.Text;
+            db.SaveChanges();
+        }
+    }
+}
